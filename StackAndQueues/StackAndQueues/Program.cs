3498f98d5422c3d912d0a5aa4c880ac66ffc5927
ronit#123 <ronitwaje23>
2022-02-03 17:41:44 +0530

    create the stack

--- StackAndQueues/StackAndQueues/Program.cs
+++ StackAndQueues/StackAndQueues/Program.cs
@@ -10,8 +10,17 @@ namespace StackAndQueues
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("welcome to data structure");
+            Console.WriteLine("Welcome to Stack And Queue");
+            Stack stack = new Stack();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Display();
             Console.ReadLine();
         }
     }
 }
+
+
+
+
